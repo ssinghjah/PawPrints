9e93687ff48d0a5a28ce9ce9d0b64d59b572a851
Simran Singh <ssingh28@ncsu.edu>
2024-03-17 21:35:13 -0400

    command line arguments for pawprints to influx

--- LogAnalyzer/pawprints_to_influx.py
+++ LogAnalyzer/pawprints_to_influx.py
@@ -3,26 +3,22 @@ import time, datetime
 import os
 import pytz
 import json
+import argparse
 
-URL="http://localhost:8086"
-ORG = "aerpaw"
-TOKEN = "WhbRxhbE2aA8_IPfKp3ddov8-uwq9iS2K72l31aF9GPu4f5Me4JSQXCQ1MqhXmIVvTbsxBUCBAhTcFoa8itKUw=="
+DEFAULT_URL="http://localhost:8086"
+DEFAULT_ORG = "aerpaw"
+DEFAULT_TOKEN = "WhbRxhbE2aA8_IPfKp3ddov8-uwq9iS2K72l31aF9GPu4f5Me4JSQXCQ1MqhXmIVvTbsxBUCBAhTcFoa8itKUw=="
 
 import os, time
 from influxdb_client import InfluxDBClient, BucketsApi
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-client = InfluxDBClient(url=URL, token=TOKEN, org=ORG)
-write_api = client.write_api(write_options=SYNCHRONOUS)
-buckets_api = BucketsApi(client)
 
+DEFAULT_LOG_PATH = "./pawprints.jsonl"
+THROTTLE = 0.3 # seconds
+BUCKET_RETENTION_PERIOD = 24 * 180 # 180 days * 24 hours per day
 
-LOG_PATH = "./pawprints.jsonl"
-STREAM_INTERVAL = 1 # seconds
-SOURCE_NAME = "Helikite"
-BUCKET_RETENTION_PERIOD = 24 * 180
-
-def create_bucket_if_doesnt_exists(bucket_name):
+def create_bucket_if_doesnt_exists(buckets_api, bucket_name):
     # Check if bucket exists
     buckets = buckets_api.find_buckets().buckets
     bucket_exists = any(bucket.name == bucket_name for bucket in buckets)
@@ -37,17 +33,24 @@ def create_bucket_if_doesnt_exists(bucket_name):
         }
         buckets_api.create_bucket(bucket_properties)
 
-def read_stream_from_log():
+def run(options):
+    client = InfluxDBClient(url=options.url, token=options.token, org=options.org)
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    buckets_api = BucketsApi(client)
+
     created_buckets = []
-    bucket_name = "PawPrints"
-    with open(LOG_PATH) as f:
+    with open(options.input) as f:
         log_lines = f.readlines()
         for log_line in log_lines:
             log_line_json = json.loads(log_line)
-            # bucket_name = log_line_json["device_name"] + "-" + log_line_json["campaign_name"]
-            
+
+            if options.bucket == None:
+                bucket_name = log_line_json["device_name"] + "-" + log_line_json["campaign_name"]
+            else:
+                bucket_name = options.bucket_name
+
             if bucket_name not in created_buckets:
-                create_bucket_if_doesnt_exists(bucket_name)
+                create_bucket_if_doesnt_exists(buckets_api, bucket_name)
                 created_buckets.append(bucket_name)
 
             abs_time = log_line_json["abs_time"]
@@ -77,47 +80,34 @@ def read_stream_from_log():
                         influx_data["fields"][key] = cell[key]
 
 
-            write_api.write(bucket=bucket_name, token=TOKEN, org=ORG, record=influx_data)
+            write_api.write(bucket=bucket_name, token=options.token, org=options.org, record=influx_data)
 
             if cell == log_line_json["connected_pci"]:
                 influx_data["tags"]["pci"] = "connected"
-                write_api.write(bucket=bucket_name, token=TOKEN, org=ORG, record=influx_data)
-                    
-            time.sleep(0.3)
+                write_api.write(bucket=bucket_name, token=options.token, org=options.org, record=influx_data)
+            
+            if options.verbose:
+                print(f"Writing time step: {rel_time} s")
+            time.sleep(THROTTLE)
 
-# print(created_buckets)
-        # influx_data["tags"]["type"] = "ue"
-        # influx_data["fields"]["lat"] = ue["current_position_lla"][0]
-        # influx_data["fields"]["lon"] = ue["current_position_lla"][1]
-        # influx_data["fields"]["alt"] = float(ue["current_position_lla"][2])
-        # write_api.write(bucket=BUCKET, token=TOKEN, org=ORG, record = data)
-           
-          #  parse_result = library.parse_to_json(log_line)
-          #  if parse_result["status"] == 1:
-          #      log_json = parse_result["result"]
-          #      if log_json["num_cells_seen"] > 0:
-          #          for cell in log_json["cells"]:
-          #               influx_data = {}
-          #               influx_data["measurement"] = "cellular"
-          #               influx_data["fields"] = {}
-          #               influx_data["tags"] = {}
-          #               time_obj = datetime.datetime.fromtimestamp(log_json["abs_time"]/1e3)
-          #               eastern_tz = pytz.timezone('US/Eastern')
-          #               time_obj = eastern_tz.localize(time_obj)
-          #               influx_data["time"] = time_obj.isoformat()                        
-          #               influx_data["tags"]["cell_pci"] = cell
-          #               for kpi in log_json["cells"][cell]:
-          #                 influx_data["fields"][kpi] = log_json["cells"][cell][kpi]
-          #               print(influx_data)
-          #               print(write_api.write(bucket=BUCKET, token=TOKEN, org=ORG, record = influx_data))
+    print("Created buckets:")
+    print(created_buckets)
 
-          #               if log_json["cells"][cell]["is_connected"] == "TRUE":
-          #                 influx_data["tags"]["cell_pci"] = "connected"
-          #                 print(write_api.write(bucket=BUCKET, token=TOKEN, org=ORG, record = influx_data))
-
-                
-
-                           
-        #   
-
-read_stream_from_log()
+if __name__ == "__main__":
+    options = argparse.ArgumentParser(description="Write PawPrints log to Influx database")
+    options.add_argument('-v', '--verbose', action='store_true', default=False, 
+                        help="Print progress on console")
+    options.add_argument('-b', '--bucket', type = str, default=None,
+                        help='Bucket name. By default, a combination of campaign name and device name is used.')
+    options.add_argument('-t', '--token', type = str, default = DEFAULT_TOKEN,
+                            help='Influx token')
+    options.add_argument('-u', '--url', type = str, default = DEFAULT_URL,
+                            help='Influx URL. Default is http://localhost:8086.')
+    options.add_argument('-o', '--org', type = str, default = DEFAULT_ORG,
+                            help='Influx organization. Default is aerpaw.')
+    options.add_argument('-i', '--input', type = str, default = DEFAULT_LOG_PATH,
+                            help='PawPrints log path. Default is ./pawprints.jsonl')
+    options = options.parse_args()
+    run(options)
+    print("Bucket name: ")
+    print(options.bucket)
