4b737c7af934f31df771b27232ca56a1144c51f7
Simran Singh <ssingh28@ncsu.edu>
2024-04-11 00:16:29 -0400

    pawprints to csv

--- LogAnalyzer/pawprints_to_csv.py
+++ LogAnalyzer/pawprints_to_csv.py
@@ -6,15 +6,24 @@ import json
 import pandas
 import argparse
 
-OUTPUT_FOLDER = "./WorkSpace/Data/"
+OUTPUT_FOLDER = "../../WorkArea/"
 
 # Default Options
-PAWPRINTS_LOG_PATH = "./pawprints.jsonl"
-MERGE_DEFAULT = "all" # Can be all or per-cell or per-cell-kpi
+PAWPRINTS_LOG_PATH = "../../WorkArea/pawprints.jsonl"
+MERGE_DEFAULT = "per-cell-kpi" # Can be all or per-cell or per-cell-kpi
 OUTPUT_CSV_PREFIX = ""
+OUTPUT_DEFAULT = "csv"
 
 def append_to_csv(path, single_row):
     with open(path, 'a') as f_pointer:
+        csv_pointer = csv.writer(f_pointer)
+        if isinstance(single_row, list):
+            csv_pointer.writerow(single_row)
+        else:
+            csv_pointer.writerow([single_row])
+
+def write_to_csv(path, single_row):
+    with open(path, 'w+') as f_pointer:
         csv_pointer = csv.writer(f_pointer)
         if isinstance(single_row, list):
             csv_pointer.writerow(single_row)
@@ -24,7 +33,8 @@ def append_to_csv(path, single_row):
 def get_csv_kpi_path(pci, kpi):
     return os.path.join(OUTPUT_FOLDER, OUTPUT_CSV_PREFIX + str(pci) + "_" + kpi + ".csv")
     
-def process_log_row(log_row, csv_data, options):
+def process_log_row(log_row, data, options):
+    log_row["type"] = "cellular" # Temp fix. Change upstream
     if log_row["type"] == "cellular":
         # Process cellular log
         if options.mergemode == "per-cell-kpi":
@@ -34,21 +44,28 @@ def process_log_row(log_row, csv_data, options):
             for cell in log_row["cells"]:
                     for kpi in cell:
                         if kpi != "pci":
-                            append_to_csv(get_csv_kpi_path(cell["pci"], kpi), cell[kpi])
-                    append_to_csv(get_csv_kpi_path(cell["pci"], "abs_time"), abs_time)
-                    append_to_csv(get_csv_kpi_path(cell["pci"], "rel_time"), rel_time)
+                            key = cell["pci"] + "_" + kpi
+                            if key not in data:
+                                data[key] = []
+                            data[key].append(cell[kpi])
+                    #         append_to_csv(get_csv_kpi_path(cell["pci"], kpi), cell[kpi])
+                    # append_to_csv(get_csv_kpi_path(cell["pci"], "abs_time"), abs_time)
+                    # append_to_csv(get_csv_kpi_path(cell["pci"], "rel_time"), rel_time)
 
             # Write connected cell logs
             connected_pci = log_row["connected_pci"]
             append_to_csv(get_csv_kpi_path("connected", "pci"), connected_pci)
             append_to_csv(get_csv_kpi_path("connected", "abs_time"), abs_time)
             append_to_csv(get_csv_kpi_path("connected","rel_time"), rel_time)
-            connected_index = log_row["connected_index"]
+            connected_index = 0
+            #connected_index = log_row["connected_index"]
             if connected_pci != -1:
                 connected_cell = log_row["cells"][connected_index]
                 for kpi in connected_cell:
-                    if kpi != "pci":
-                        append_to_csv(get_csv_kpi_path('connected', kpi), connected_cell[kpi])
+                        if "connected_" + kpi not in data:
+                            data["connected_" + kpi] = []
+                        data["connected_" + kpi].append(connected_cell[kpi])
+                        # append_to_csv(get_csv_kpi_path('connected', kpi), connected_cell[kpi])
 
         elif options.mergemode == "all":
             for cell in log_row["cells"]:
@@ -64,23 +81,8 @@ def process_log_row(log_row, csv_data, options):
                 if cell["pci"] == log_row["connected_pci"]:
                     panda_cell["is_connected"] = 1 
 
-                csv_data["data"] = pandas.concat([csv_data["data"], pandas.DataFrame(panda_cell, index = [0])], ignore_index = True)
+                data["data"] = pandas.concat([data["data"], pandas.DataFrame(panda_cell, index = [0])], ignore_index = True)
 
-
-                # pci = cell["pci"]
-                # for kpi in cell:
-                #     if kpi != "pci":
-                #         pd_json[str(pci) + "_" + kpi] = cell[kpi]
-
-            # connected_pci = log_row["connected_pci"]
-            # connected_index = log_row["connected_index"]
-            # if connected_pci != -1:
-            #     connected_cell = log_row["cells"][connected_index]
-            #     for kpi in connected_cell:
-            #         if kpi != "pci":
-            #             pd_json["connected_" + kpi] = cell[kpi]
-            
-        
     # Write metadata about seen cells
     seen_pcis = []
     for cell in log_row["cells"]:
@@ -92,18 +94,26 @@ def process_log_row(log_row, csv_data, options):
 def process_log(options):
     handover_count = 0
     
-    csv_data = {"name":"", "data": None}
+    data = {}
     if options.mergemode== "all":
-        csv_data["data"] = pandas.DataFrame(index=None)
+        data["data"] = pandas.DataFrame(index=None)
     
     with open(PAWPRINTS_LOG_PATH) as f:
         log_rows = f.readlines()
         for log_row_string in log_rows:
            log_row = json.loads(log_row_string)
-           process_log_row(log_row, csv_data, options)
+           process_log_row(log_row, data, options)
+    
+    if options.output == "csv" and options.mergemode == "all":
+        data["data"].to_csv(os.path.join(OUTPUT_FOLDER, "pawprints.csv"))
+    if options.output == "csv" and options.mergemode == "per-cell-kpi":
+        for key in data:
+            write_to_csv(os.path.join(OUTPUT_FOLDER, key + ".csv"),data[key])
 
-    if options.mergemode == "all":
-        csv_data["data"].to_csv(os.path.join(OUTPUT_FOLDER, "pawprints.csv"))
+
+
+    
+    
     '''
     summary = {}
     summary["unique_num_cells_seen"] = 
@@ -124,6 +134,8 @@ def main():
     parser.add_argument('-p', '--csvprefix', type=str, default=OUTPUT_CSV_PREFIX, help="Prefix of the output csv file(s)")
     parser.add_argument('-m', '--mergemode', type = str, default = MERGE_DEFAULT,
                         help='Options to merge the output CSV. "all" = creates one csv containing all kpis of all cell. "per-cell-kpi" = creates multiple csvs, one for each kpi of all cell')
+    parser.add_argument('-o', '--output', type = str, default = OUTPUT_DEFAULT,
+                        help='Output options. csv or influx')
     options = parser.parse_args()
     process_log(options)
 
